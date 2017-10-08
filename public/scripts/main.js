23aff801e2b674f78622c2697ad4949653e04f9c
justintlai <justintlai@hotmail.co.uk>
2017-10-09 00:43:47 +0100

    modified the data output

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
       beforeSend: function() {
         // this is where we append a loading image
         $('#ajax-panel').html(
-          '<div class="loading"><img src="/img/loading.gif" alt="Loading..." /></div>'
+          '<div class="loading"><img class="img-fluid" src="/img/loading.gif" alt="Loading..." /></div>'
         );
       },
       type: 'POST',
@@ -20,15 +20,34 @@ $(document).ready(function() {
         console.log(result);
         $('#ajax-panel').empty();
         for (var i = 0; i < result.length; i++) {
-          $('#result').append(result[i].link_text + '</br>');
           $('#result').append(
-            '<a href=' + result[i].link + '>Source<a>' + '</br>'
+            '<div class="card">' +
+              '<h3 class="card-header">' +
+              result[i].source +
+              '<div class="card-block"></h3>' +
+              '<p class="card-text"><a href=' +
+              result[i].link +
+              '>Source</a></p>' +
+              '<p class="card-text">' +
+              new Date(parseInt(result[i].time) * 1000) +
+              '</p>' +
+              '<p class="card-text">' +
+              result[i].link_text +
+              '</p>' +
+              '</div></div>'
           );
-          $('#result').append(new Date(result[i].time * 1000) + '</br>');
-          $('#result').append(result[i].source + '</br>');
-          $('#result').append('</br></br>');
         }
       },
+
+      // <div class="card">
+      //   <h3 class="card-header">Featured</h3>
+      //   <div class="card-block">
+      //     <h4 class="card-title">Special title treatment</h4>
+      //     <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+      //     <a href="#" class="btn btn-primary">Go somewhere</a>
+      //   </div>
+      // </div>
+
       error: function(error) {
         console.log(error);
         // failed request; give feedback to user
